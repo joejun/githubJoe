d57db3fd9b73c66bbca840066e3857655a315bdf
joejun <94554375@qq.com>
2015-11-30 10:38:45 +0800

    my util
    my util

---
+++ joeUtil/src/main/java/com/joe/javaprj/joeUtil/App.java
@@ -0,0 +1,13 @@
+package com.joe.javaprj.joeUtil;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
